c6556749fc869dfdf333b86ffe932cdb01f84a1d
Kaeden <kaedencraft@gmail.com>
2026-08-26 18:13:50 -0400

    Fixed missing server favicon

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>{{ env('APP_NAME') }}</title>
+    <title>{{ config('app.name') }} - The brand new elemental trading card game!</title>
+    <link rel="icon" href="/favicon.png" type="image/x-icon">
     @vite(['resources/css/style.css'])
     @stack('css')
     @stack('js')
