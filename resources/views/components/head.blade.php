dbd1b98c8f44dec0b9facd5199cc6280d45ea0b3
Kaeden <kaedencraft@gmail.com>
2026-08-28 16:06:01 -0400

    Added meta description

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="An easy-to-learn trading card game that pits decks of creatures against each other to boost their power to victory." />
     <title>{{ config('app.name') }} - The brand new elemental trading card game!</title>
     <link rel="icon" href="/favicon.png" type="image/x-icon">
     @vite(['resources/css/style.css'])
